65efa637e115c3ca8c7a4b700b1036584f0bb3bc
Maksim Nemesh <user.email>
2023-08-17 00:17:03 +0300

    add GET-request

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,8 +1,17 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld() {
+        //Ex3: Создание репозитория
         System.out.println("Hello from Maks");
+        //Ex4: GET-запрос
+        RestAssured.useRelaxedHTTPSValidation();
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
     }
 }
